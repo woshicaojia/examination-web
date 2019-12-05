b2d95dd17b0e92ed6e9f46edfe07f6a3cda1dea7
woshicaojia <18894037898@163.com>
2019-12-05 15:13:29 +0800

    vue项目web端

---
+++ src/main/java/com/example/examonlineweb/ExamOnlineWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.examonlineweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamOnlineWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamOnlineWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/examonlineweb/ExamOnlineWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examonlineweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamOnlineWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
